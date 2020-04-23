d291d931af768fc48ef196b733c72f07a08931fb
Matt Campbell <Matthew.Campbell@raytheon.com>
2020-04-23 09:26:15 -0400

    add deref package

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Restivus [v1.0.0](https://github.com/maka-io/meteor-restivus/blob/devel/CHANGELOG.md#change-log) [![Build Status](https://travis-ci.org/kahmali/meteor-restivus.svg)](https://travis-ci.org/kahmali/meteor-restivus)
+# Restivus [v1.0.1](https://github.com/maka-io/meteor-restivus/blob/devel/CHANGELOG.md#change-log) [![Build Status](https://travis-ci.org/kahmali/meteor-restivus.svg)](https://travis-ci.org/kahmali/meteor-restivus)
 
 #### REST APIs for the Best of Us!
 
--- package.js
+++ package.js
@@ -1,32 +1,34 @@
 Package.describe({
     name: 'maka:rest',
     summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
-    version: '1.0.0',
+    version: '1.0.1',
     git: 'https://github.com/maka-io/maka-rest.git'
 });
 
 Npm.depends({
-  "url-parse": "1.1.7"
+    'url-parse': '1.1.7',
+    '@apidevtools/json-schema-ref-parser': '9.0.1',
+    'yamljs': '0.3.0'
 });
 
 Package.onUse(function (api) {
-  // Minimum Meteor version
-  api.versionsFrom('METEOR@1.10.2');
+    // Minimum Meteor version
+    api.versionsFrom('METEOR@1.10.2');
 
-  // Meteor dependencies
-  api.use('check');
-  api.use('underscore');
-  api.use('ecmascript');
-  api.use('accounts-password@1.3.3');
-  api.use('simple:json-routes@2.1.0');
+    // Meteor dependencies
+    api.use('check');
+    api.use('underscore');
+    api.use('ecmascript');
+    api.use('accounts-password@1.3.3');
+    api.use('simple:json-routes@2.1.0');
 
-  //api.addFiles('lib/auth.js', 'server');
-  //api.addFiles('lib/route.js', 'server');
-  //api.addFiles('lib/restivus.js', 'server');
-  // api.addFiles('lib/restivus-swagger.js', 'server');
+    //api.addFiles('lib/auth.js', 'server');
+    //api.addFiles('lib/route.js', 'server');
+    //api.addFiles('lib/restivus.js', 'server');
+    // api.addFiles('lib/restivus-swagger.js', 'server');
 
-  // Exports
-  //api.export('Restivus', 'server');
+    // Exports
+    //api.export('Restivus', 'server');
     api.mainModule('lib/restivus.js', 'server');
 });
 
