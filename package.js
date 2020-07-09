2fbe5e7485ea39eab60bedc1a7482d7a1611a325
maka-io <mjmcee@gmail.com>
2020-07-09 11:33:01 -0400

    add auth

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'maka:rest',
     summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
-    version: '1.0.3',
+    version: '1.0.4',
     git: 'https://github.com/maka-io/maka-rest.git'
 });
 
