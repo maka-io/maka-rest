c6f90400441d440dec20bca017869668f661d01a
maka-io <mjmcee@gmail.com>
2021-07-13 13:09:59 -0400

    allow both x-auth-token and X-Auth-Token

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'maka:rest',
     summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
-    version: '2.0.6',
+    version: '2.0.7',
     git: 'https://github.com/maka-io/maka-rest.git'
 });
 
