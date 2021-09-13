4f4ddd62c7f7212d030c4b4fa753cb0257445cb8
Matt Campbell <maka-io@users.noreply.github.com>
2021-09-13 09:30:30 -0400

    Update README.md

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'maka:rest',
     summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
-    version: '2.0.11',
+    version: '2.0.12',
     git: 'https://github.com/maka-io/maka-rest.git'
 });
 
