be90547e357f8b4ec302dfdf4a6b5efac794f1cd
maka-io <mjmcee@gmail.com>
2021-09-27 16:01:27 -0400

    clearer auth handling

--- package.js
+++ package.js
@@ -1,36 +1,36 @@
 Package.describe({
-    name: 'maka:rest',
-    summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
-    version: '2.0.11',
-    git: 'https://github.com/maka-io/maka-rest.git'
+  name: 'maka:rest',
+  summary: 'Create authenticated REST APIs in Meteor 1.10.2+ via HTTP/HTTPS',
+  version: '2.0.11',
+  git: 'https://github.com/maka-io/maka-rest.git'
 });
 
 Npm.depends({
-    'url-parse': '1.1.7',
-    '@apidevtools/json-schema-ref-parser': '9.0.1',
-    'yamljs': '0.3.0',
-    'limiter': '1.1.5'
+  'url-parse': '1.1.7',
+  '@apidevtools/json-schema-ref-parser': '9.0.1',
+  'yamljs': '0.3.0',
+  'limiter': '1.1.5'
 });
 
 Package.onUse(function (api) {
-    // Minimum Meteor version
-    api.versionsFrom(['1.10.2','2.3']);
+  // Minimum Meteor version
+  api.versionsFrom(['1.10.2','2.3']);
 
-    // Meteor dependencies
-    api.use('check');
-    api.use('underscore');
-    api.use('ecmascript');
-    api.use('accounts-password');
-    api.use('simple:json-routes@2.1.0');
+  // Meteor dependencies
+  api.use('maka:check');
+  api.use('underscore');
+  api.use('ecmascript');
+  api.use('accounts-password');
+  api.use('simple:json-routes@2.1.0');
 
-    //api.addFiles('lib/auth.js', 'server');
-    //api.addFiles('lib/route.js', 'server');
-    //api.addFiles('lib/restivus.js', 'server');
-    // api.addFiles('lib/restivus-swagger.js', 'server');
+  //api.addFiles('lib/auth.js', 'server');
+  //api.addFiles('lib/route.js', 'server');
+  //api.addFiles('lib/restivus.js', 'server');
+  // api.addFiles('lib/restivus-swagger.js', 'server');
 
-    // Exports
-    //api.export('Restivus', 'server');
-    api.mainModule('lib/restivus.js', 'server');
+  // Exports
+  //api.export('Restivus', 'server');
+  api.mainModule('lib/restivus.js', 'server');
 });
 
 Package.onTest(function (api) {
