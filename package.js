fe0234a7dbd2f0d31818df974ab210668b583de2
maka-io <mjmcee@gmail.com>
2023-12-28 13:44:55 -0500

    bump ver

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'maka:rest',
   summary: 'Create authenticated REST APIs in Meteor 2.13+ via HTTP/HTTPS',
-  version: '4.0.2',
+  version: '4.0.3',
   git: 'https://github.com/maka-io/maka-rest.git'
 });
 
